7defdad79df417dd61dc2912d5da4a1fb5e0099e
CheNgH826 <lilioo826@gmail.com>
2017-10-15 20:50:02 +0800

    finish logistic training

---
+++ hw2/testing.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+x_data = pd.read_csv('data/X_train', header='false')
+y_data = x_data = pd.read_csv('data/Y_train', header='false')
---
+++ hw2/util.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def standardize(matrix): # matrix.shape = (m, n)
+    avg = np.mean(matrix, axis=0)
+    std = np.std(matrix, axis=0)
+    return (matrix-avg)/std
+
+def cross_entropy(p,q):
+    return -(p*np.log(q)+(1-p)*np.log(1-q))
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def determine_ans(f):
+    ans = np.zeros(f.shape)
+    for i in range(len(f)):
+        if f[i] >= 0.5:
+            ans[i]=1
+    return ans.astype(int)
+
+
+def compute_accuracy(f,y):
+    ans = determine_ans(f)
+    return np.sum(np.logical_not(np.logical_xor(ans,y)))/len(f)
