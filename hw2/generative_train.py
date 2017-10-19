1a1ea10b769961744aaab33770cd1685e825b6f6
CheNgH826 <lilioo826@gmail.com>
2017-10-19 10:52:09 +0800

    generative unfinished

--- hw2/generative_train.py
+++ hw2/generative_train.py
@@ -4,6 +4,7 @@ from util import *
 
 def likelihood(avg, cova, x):
     inv_cova = np.linalg.inv(cova)
+    # print(np.linalg.det(cova))
     const = ((2*np.pi)**cova.shape[0]*np.linalg.det(cova))**(-0.5)
     exp_power = -0.5*np.dot(np.dot(x-avg, np.linalg.inv(cova)), (x-avg).T)
     return const*np.exp(exp_power)
@@ -18,7 +19,9 @@ y_train = np.array(pd.read_csv('data/proc_y_train'))
 
 class1_size = np.sum(y_train)
 class0_size = y_train.shape[0]-class1_size
-print(class0_size, class1_size)
+# print(class0_size, class1_size)
+prob_0 = float(class0_size/y_train.shape[0])
+prob_1 = 1-prob_0
 class0_data = []
 class1_data = []
 
@@ -36,15 +39,17 @@ avg1 = np.mean(class1_data, axis=0)
 
 cova0 = np.dot((class0_data-avg0).T, class0_data-avg0)/class0_data.shape[0]
 cova1 = np.dot((class1_data-avg0).T, class1_data-avg0)/class1_data.shape[0]
-print(cova1.shape)
-cova = (cova0*class0_size+cova1*class1_size)/(class0_size+class1_size)
+# print(cova1.shape)
+cova = cova0*prob_0+cova1*prob_1
+
 
 # validation
 x_va = np.array(pd.read_csv('data/proc_x_va'))
 y_va = np.array(pd.read_csv('data/proc_y_va'))
-ans = np.empty(y_va.shape)
+ans = np.zeros(y_va.shape)
 
 for i in range(x_va.shape[0]):
-    ans[i] = determine_ans(posterier(x_va[i]))
+    if posterier(avg0, cova, x_va[i], prob_0, avg1) < posterier(avg1, cova, x_va[i], prob_0, avg0):
+        ans[i]=1
 
 print(compute_accuracy(ans, y_va))
--- hw2/try.py
+++ hw2/try.py
@@ -1,8 +1,7 @@
 import numpy as np
 from util import *
 
-a = np.array([0.1,0.2,.3,.4,.6,.3,.8])
-b = np.array([0,1,0,1,1,1,1])
-print(determine_ans(a))
-print(b)
-print(compute_accuracy(a,b))
+a = np.ones((2,3))
+b = np.ones((3,2))
+print(np.dot(a,b))
+print(a*b)
