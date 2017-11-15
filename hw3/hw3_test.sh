d075d2a1ee129c2165c1e04bb0cd827b9f465352
CheNgH826 <lilioo826@gmail.com>
2017-11-15 20:02:48 +0800

    update wget url

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/CheNgH826/ML2017FALL/releases/download/hw3_model/model.hdf5
+wget https://github.com/CheNgH826/hello-world/releases/download/mlhw3/model.hdf5
 python3 test.py $1 $2
