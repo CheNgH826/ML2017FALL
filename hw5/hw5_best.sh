d83bc77d6da5b04f0613044c130b948aba81fd2b
CheNgH826 <lilioo826@gmail.com>
2017-12-16 23:04:52 +0800

    remove tmp ans

--- hw5/hw5_best.sh
+++ hw5/hw5_best.sh
@@ -4,3 +4,4 @@ python3 test.py $1 model_nobias ans_nobias
 python3 test.py $1 model_bias_256 ans_bias_256
 python3 test.py $1 model_nobias_256 ans_nobias_256
 python3 ensemble.py ans_bias ans_nobias ans_bias_256 ans_nobias_256 $2
+rm ans_bias ans_nobias ans_bias_256 ans_nobias_256
