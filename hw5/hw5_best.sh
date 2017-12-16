fa907dad0e2c7c113e898844542fc71cc9e30fd4
CheNgH826 <lilioo826@gmail.com>
2017-12-16 22:06:31 +0800

    script upload

---
+++ hw5/hw5_best.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 test.py $1 model_bias ans_bias
+python3 test.py $1 model_nobias ans_nobias
+python3 test.py $1 model_bias_256 ans_bias_256
+python3 test.py $1 model_nobias_256 ans_nobias_256
+python3 ensemble.py ans_bias ans_nobias ans_bias_256 ans_nobias_256 $2
